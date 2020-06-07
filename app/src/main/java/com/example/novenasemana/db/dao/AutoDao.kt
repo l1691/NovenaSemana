d95ccf5c6e0fd65d9095bfe7b1a5270014d6899a
programador7@dsbmobile.com <rvaldiviamalaga@gmail.com>
2020-06-06 22:00:33 -0500

    insert auto

--- app/src/main/java/com/example/novenasemana/db/dao/AutoDao.kt
+++ app/src/main/java/com/example/novenasemana/db/dao/AutoDao.kt
@@ -1,14 +1,19 @@
 package com.example.novenasemana.db.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.novenasemana.db.entity.AutoEntity
 
 @Dao
 interface AutoDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insertAuto(auto: AutoEntity)
+    suspend fun insertAuto(auto: AutoEntity)
+
+    @Query("SELECT * FROM auto")
+    fun getAutos():LiveData<List<AutoEntity>>
 
 }
 
--- app/src/main/java/com/example/novenasemana/repository/AutoRepository.kt
+++ app/src/main/java/com/example/novenasemana/repository/AutoRepository.kt
@@ -1,10 +1,14 @@
 package com.example.novenasemana.repository
 
+import androidx.lifecycle.LiveData
 import com.example.novenasemana.db.dao.AutoDao
 import com.example.novenasemana.db.entity.AutoEntity
 
 class AutoRepository (private val autoDao:AutoDao){
-    fun insertAuto(auto:AutoEntity){
+
+    val autos:LiveData<List<AutoEntity>> = autoDao.getAutos()
+
+    suspend fun insertAuto(auto:AutoEntity){
         autoDao.insertAuto(auto)
     }
 }
